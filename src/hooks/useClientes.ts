60d5cb61ddfe3dc7d4dc9e85be48582cc7a39a5e
Kaio Dantas Mancini <113388395+kaio-d@users.noreply.github.com>
2023-06-12 18:05:09 -0300

    inital commit

---
+++ src/hooks/useClientes.ts
@@ -0,0 +1,54 @@
+import Cliente from "../core/Cliente";
+import { useEffect, useState } from "react";
+import ClienteRepositorio from "@/core/ClienteRepositorio";
+import ColecaoCliente from "@/backend/db/ColecaoCliente";
+import useTabelaOuForm from "./useTabelaOuForm";
+
+export default function useClientes() {
+    const repo: ClienteRepositorio = new ColecaoCliente()
+    const { tabelaVisivel, formularioVisivel, exibirTabela, exibirFormulario } = useTabelaOuForm()
+    const [cliente, setCliente] = useState<Cliente>(Cliente.vazio());
+    const [clientes, setClientes] = useState<Cliente[]>([]);
+
+    useEffect(obterTodos, [])
+
+    function obterTodos() {
+        repo.obterTodos().then(clientes => {
+            setClientes(clientes)
+            exibirTabela()
+        })
+
+    }
+
+    function selecionarCliente(cliente: Cliente) {
+        setCliente(cliente);
+        exibirFormulario();
+    }
+
+    async function excluirCliente(cliente: Cliente) {
+        await repo.excluir(cliente)
+        obterTodos();
+    }
+
+    function novoCliente() {
+        setCliente(Cliente.vazio());
+        exibirFormulario()
+    }
+
+    async function salvarCliente(cliente: Cliente) {
+        await repo.salvar(cliente)
+        obterTodos();
+    }
+
+    return {
+        cliente,
+        clientes,
+        obterTodos,
+        selecionarCliente,
+        excluirCliente,
+        novoCliente,
+        salvarCliente,
+        tabelaVisivel,
+        exibirTabela
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,17 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+  safelist: [
+    {
+      pattern: /bg-(green|blue|gray)-(400|700)/,
+    },{
+      pattern: /from-(green|blue|gray)-(400|700)/,
+    },{
+      pattern: /to-(green|blue|gray)-(400|700)/,
     },
+  ],
+  theme: {
+    extend: {},
   },
   plugins: [],
 }
